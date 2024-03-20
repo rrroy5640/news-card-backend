fa37ab0543db36476cb7be2cdbf019e175a58029
rrroy <hly5640@gmail.com>
2024-03-20 15:21:27 +1100

    added cors middleware

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 8000;
+const cors = require("cors");
 
-app.use(express.json({ extended: false}));
-
+app.use(express.json({ extended: false }));
+app.use(cors());
 app.use("/api", require("./routes/api/news"));
 
 app.listen(port, () => {
